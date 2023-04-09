bca1edd51f9e5f94a7254b41a5dfcd222349140a
bpmo1234 <suvproduction@gmail.com>
2023-04-09 21:24:40 +0200

    1

--- scriptl.js
+++ scriptl.js
@@ -162,31 +162,27 @@ const currpopularfun = (movie) => {
              </div>
          </div>`;
 };
-const topratedmoviesfun = (tv) => {
-  let url = "./tvshowsDetail.html?id=" + encodeURIComponent(movie.id);
-  return `<div class="item" >
-    <a class="posterlink" href="${url}"> <img class="poster" data-id="${
+const topratedmoviesfun = (movie) => {
+  let url = "./TvshowsDetails.html?id=" + encodeURIComponent(movie.id);
+  return `<div class="Top_rated_movies" >
+    <a class="posterlink" href=${url}> <img class="poster" data-id="${
     movie.id
-  }" 
-  src='./resources/D moviesand tv show.png'
-  data-src="https://image.tmdb.org/t/p/w500/${movie.poster_path}"
-  loading="lazy" 
-  onload="this.src=this.getAttribute('data-src')"
- 
-        alt="${movie.name}"></a>
-         <p class="movie_title movie_title_search" >${movie.name}</p>
-         <div class="date_rating tvshows_date_rating">
-             <p class="date date_search">${dateFormatter(
+  }" src="https://image.tmdb.org/t/p/w500/${movie.poster_path}"loading="lazy" 
+  onerror="this.onerror=null;this.src='./resources/D moviesand tv show.png';"
+  alt="${movie.name}"></a>
+         <p class="movie_title">${movie.name}</p>
+         <div class="date_rating">
+             <p class="date">${dateFormatter(
                movie.first_air_date
-             )}</p><span class="dot dot2 recommendTvShow_date_dot"></span>
-             <p class="rating rating_search">${
+             )}</p><span class="dot dot2"></span>
+             <p class="rating">${
                movie.vote_average
              }<span><svg xmlns="http://www.w3.org/2000/svg" width="10"
                          height="10" fill="Yellow" class="star bi-star-fill" viewBox="0 0 16 16">
                          <path
                              d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z" />
                      </svg></span></p>
-             <div class="category category_search recommendTvShow_category">TV show</div>
+             <div class="category">TV Show</div>
              </div>
          </div>`;
 };
