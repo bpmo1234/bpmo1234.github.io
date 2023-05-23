2b71735ecfad7fe922360561e73829d7fd940fd5
bpmo1234 <suvproduction@gmail.com>
2023-05-23 08:14:23 +0000

    Update api.js

--- js/api.js
+++ js/api.js
@@ -1,4 +1,4 @@
-let api_key = "25986140e0d34941636f99dbf6f5310e";
+let api_key = "8cab626c05f8766826a37e476d07b229";
 
 let img_url = "https://image.tmdb.org/t/p/w500";
 let original_img_url = "https://image.tmdb.org/t/p/original";
