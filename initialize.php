9744dbc01b16810b29d660aac31a021be339dcf1
bpmo1234 <67483858+bpmo1234@users.noreply.github.com>
2024-08-07 14:19:26 +0000

    Update initialize.php

--- initialize.php
+++ initialize.php
@@ -1,12 +1,12 @@
 <?php
 $dev_data = array('id'=>'-1','firstname'=>'Developer','lastname'=>'','username'=>'dev_oretnom','password'=>'5da283a2d990e8d8512cf967df5bc0d0','last_login'=>'','date_updated'=>'','date_added'=>'');
-if(!defined('base_url')) define('base_url','sgisz.unaux.com/');
+if(!defined('base_url')) define('base_url','https://bpmo1234.github.io/');
 if(!defined('base_app')) define('base_app', str_replace('\\','/',__DIR__).'/' );
 if(!defined('dev_data')) define('dev_data',$dev_data);
-if(!defined('DB_SERVER')) define('DB_SERVER',"sql210.ezyro.com");
-if(!defined('DB_USERNAME')) define('DB_USERNAME',"ezyro_37055079");
-if(!defined('DB_PASSWORD')) define('DB_PASSWORD',"sangoku1234");
-if(!defined('DB_NAME')) define('DB_NAME',"ezyro_37055079_mmts");
+if(!defined('DB_SERVER')) define('DB_SERVER',"sql.freedb.tech");
+if(!defined('DB_USERNAME')) define('DB_USERNAME',"freedb_bpmo1234");
+if(!defined('DB_PASSWORD')) define('DB_PASSWORD',"z4eXxKw5H5?U2w&");
+if(!defined('DB_NAME')) define('DB_NAME',"freedb_mtmsdatabase");
 ?>
 
 <script>
