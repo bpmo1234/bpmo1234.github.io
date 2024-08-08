9e336a680e4833a6a699cb769a5681a89ae5c960
bpmo1234 <67483858+bpmo1234@users.noreply.github.com>
2024-08-08 20:43:48 +0000

    Update initialize.php

--- initialize.php
+++ initialize.php
@@ -1,24 +1,10 @@
 <?php
 $dev_data = array('id'=>'-1','firstname'=>'Developer','lastname'=>'','username'=>'dev_oretnom','password'=>'5da283a2d990e8d8512cf967df5bc0d0','last_login'=>'','date_updated'=>'','date_added'=>'');
-if(!defined('base_url')) define('base_url','https://bpmo1234.github.io/');
+if(!defined('base_url')) define('base_url','http://cinemafree.rf.gd/');
 if(!defined('base_app')) define('base_app', str_replace('\\','/',__DIR__).'/' );
 if(!defined('dev_data')) define('dev_data',$dev_data);
-if(!defined('DB_SERVER')) define('DB_SERVER',"sql.freedb.tech");
-if(!defined('DB_USERNAME')) define('DB_USERNAME',"freedb_bpmo1234");
-if(!defined('DB_PASSWORD')) define('DB_PASSWORD',"z4eXxKw5H5?U2w&");
-if(!defined('DB_NAME')) define('DB_NAME',"freedb_mtmsdatabase");
+if(!defined('DB_SERVER')) define('DB_SERVER',"sql110.infinityfree.com");
+if(!defined('DB_USERNAME')) define('DB_USERNAME',"epiz_34297621");
+if(!defined('DB_PASSWORD')) define('DB_PASSWORD',"1dfuduAx4Y5jx");
+if(!defined('DB_NAME')) define('DB_NAME',"epiz_34297621_mtms_db");
 ?>
-
-<script>
-    /* old
-
-$dev_data = array('id'=>'-1','firstname'=>'Developer','lastname'=>'','username'=>'dev_oretnom','password'=>'5da283a2d990e8d8512cf967df5bc0d0','last_login'=>'','date_updated'=>'','date_added'=>'');
-if(!defined('base_url')) define('base_url','http://localhost/mtms/');
-if(!defined('base_app')) define('base_app', str_replace('\\','/',__DIR__).'/' );
-if(!defined('dev_data')) define('dev_data',$dev_data);
-if(!defined('DB_SERVER')) define('DB_SERVER',"localhost");
-if(!defined('DB_USERNAME')) define('DB_USERNAME',"root");
-if(!defined('DB_PASSWORD')) define('DB_PASSWORD',"");
-if(!defined('DB_NAME')) define('DB_NAME',"mtms_db");
-*/
-</script>
